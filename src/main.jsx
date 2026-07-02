886eda716adc6542791cde63980846dc9eee5602
mannonov0830 <kamronbekmannonov0830@gmail.com>
2026-07-02 07:41:25 +0500

    salom

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import { createRoot } from "react-dom/client";
-import "./sass/main.scss";
-import "./index.css";
+import './sass/main.scss'
+import "./style/normalizi.css";
 import App from "./App.jsx";
 
-createRoot(document.getElementById("root")).render(<App />);
+createRoot(document.getElementById("root")).render(
+  <App />
+);
